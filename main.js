824cb2d8e323d0210cc369704eae88ac3b7854e5
ShadowKshame <104213448+ShadowKshame@users.noreply.github.com>
2023-02-15 20:36:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ status1 = "";
 objects = [];
 function setup()
 {
-    canvas = createCanvas(480, 380);
+    canvas = createCanvas(280, 280);
     canvas.center();
     video = createCapture(VIDEO);
     video.size(480,380);
@@ -12,10 +12,29 @@ function setup()
 
 function draw()
 {
-    image(video, 0, 0, 480, 380);
+    image(video, 0, 0, 280, 280);
     if(status1!="")
     {
         objectDetector.detect(video,gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("name_of_objects").innerHTML = "Number of objects detected are :"+ objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label +" "+ percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(object[i].label== objectname)
+            {
+              video.stop();
+              objectDetector.detect(gotResult);
+              document.getElementById("status").innerHTML = objectname +" found";
+
+            }
+        }
     }
 }
 
@@ -33,4 +52,14 @@ function modelLoaded()
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results)
+{
+if (error)
+{
+    console.log(error);
+}
+console.log(results);
+objects = results;
 }
